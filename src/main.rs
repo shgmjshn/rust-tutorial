b78c8d92d487128e2df697e5fbd6621b71b9ec80
root <root@kyokiscientist>
2025-03-01 00:32:57 +0900

    関数の手前まで

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,11 @@ fn main() {
     a[2] = b[2];
     println!("{:?}", &a[1..3]);
 
+    pub enum Option<T> {
+        None,
+        Some(T),
+    }
+
     struct Person {
         name: String,
         age: u32,
@@ -94,13 +99,131 @@ fn main() {
 
     let v1: u64 = 10;
     let v2 = 10u64;
+
+    let number = 1;
+    if 0 < number {
+        println!("0 < number");
+    } else if number < 0 {
+        println!("number < 0");
+    } else {
+        println!("0 == number");
+    }
+
+    let number = 1;
+    let result = if 0 <= number {
+        number
+    } else {
+        -number
+    };
+
+    let mut count = 0;
+
+    let result = loop {
+        println!("count: {}", count);
+        count += 1;
+        if count == 10 {
+            break count;
+        }
+    };
+
+    let mut count = 0;
+    while count < 10 {
+        println!("count: {}", count);
+        count += 1;
+    }
+
+    let count: i32;
+
+    for count in 0..10 {
+        println!("count: {}", count);
+    }
+
+    let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    for element in &array {
+        println!("element: {}", element);
+    }
+
+    'main: loop {
+        println!("main loop start");
+        'sub: loop {
+            println!("sub loop start");
+
+            break 'main;
+
+            println!("sub loop end"); //表示されない
+        }
+        println!("main loop end"); //表示されない
+    }
+
+    let i: i32 = 1;
+    match i {
+        1 => println!("1"),
+        2 => println!("2"),
+        3 => println!("3"),
+        _ => println!("misc"),
+    }
+
+    enum Color {
+        Red,
+        Blue,
+        Green,
+    }
+
+    let c = Color::Red;
+    match c {
+        Color::Red => println!("Red"),
+        Color::Blue => println!("Blue"),
+        Color::Green => println!("Green"),
+    }
+
+    let result: Result<i32, String> = Ok(100);
+    let result_number = match result {
+        Ok(number) => number,
+        Err(message) => {
+            println!("Error: {}", message);
+            -1
+        },
+    };
+
+    for number in 1..5 {
+        println!("{}", number);
+    }
+
+    let it = Iter {
+        current: 0,
+        max: 10,
+    };
+    for num in it {
+        println!("{}", num);
+    }
+
+    let mut v = vec![1, 2, 3, 4, 5];
+    for i in 0..v.len() {
+        v[i] += 1;
+    }
+    
 }
 
 fn print(s: Box<[u8]>) {
     println!("{:?}", s);
 }
 
-pub enum Option<T> {
-    None,
-    Some(T),
+struct Iter {
+    current: usize,
+    max: usize,
+}
+
+impl Iterator for Iter {
+    type Item = usize; //出力する型の紐づけ
+
+    //next()メソッドの実装
+    fn next(&mut self) -> Option<usize> {
+        self.current += 1;
+        if self.current - 1 < self.max {
+            Some(self.current - 1)
+        } else {
+            None
+        } 
+    }
 }
